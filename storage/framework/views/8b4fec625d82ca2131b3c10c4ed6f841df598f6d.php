59dc4ee7c1ffefcfe30c718b45594bf4b13d9ca1
luisheleon <luishele2@gmail.com>
2017-08-15 21:04:47 -0500

    Actualización de todo el directorio

---
+++ storage/framework/views/8b4fec625d82ca2131b3c10c4ed6f841df598f6d.php
@@ -0,0 +1,12 @@
+<?php $__env->startSection('titulo','Factores'); ?>
+
+<?php $__env->startSection('content'); ?>
+
+    <center><h1>Factores</h1></center>
+
+    <?php $__env->stopSection(); ?>
+
+
+
+
+<?php echo $__env->make('layouts.app', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
